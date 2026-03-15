5a6dbd65fe5f0e9a344df030a2fa08d6dab295b3
s0Fi98 <solmansofihussain@gmail.com>
2026-03-16 04:34:42 +0530

    file name modified

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,9 +1,9 @@
 import HeroSection from "@/components/heroSection";
-import AvailableCountries from "@/components/availableCountries";
-import WhyAbroad from "@/components/whyAbroad";
+import AvailableCountries from "@/components/availablecountries";
+import WhyAbroad from "@/components/whyabroad";
 import Photos from "@/components/photos";
 import Testimonials from "@/components/testimonials";
-import FAQSection from "@/components/faqSection";
+import FAQSection from "@/components/faqsection";
 
 export default function Home() {
   return (
